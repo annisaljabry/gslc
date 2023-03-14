959ae1ffffc74ccd01f812c396d1767baf4932a9
annisaljabry <annisaljabry@gmail.com>
2023-03-14 17:25:12 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="header">
+        <h1>Group 5 Profile</h1>
+    </div>
+
+    <div class="isiprofile">
+
+        <div class="profile1">
+            <div class="poto">
+                <img src="{{ asset('adit.jpg') }}" alt="">
+            </div>
+            <div class="nama">
+                <h1>Aditya Chandi Nugroho</h1>
+            </div>
+            <div class="isi">
+                <h6>Hello, my name is adit and i'm 21 years old. I am study at bina nusantara.
+                    <br>
+                    My hobby is watching tv and sleep.
+                    And my favorite course is Linear Regression.
+                </h6>
+            </div>
+        </div>
+
+        <div class="profile2">
+            <div class="poto">
+                <img src="{{ asset('keti.jpg') }}" alt="">
+            </div>
+            <div class="nama">
+                <h1>Catherine Benedicta</h1>
+            </div>
+            <div class="isi">
+                <h6>Hello, my name is kiti and i'm 20 years old. I am study at bina nusantara.
+                    <br>
+                    My hobby is watching tv and swimming.
+                    And my favorite course is Machine Learning.</h6>
+            </div>
+        </div>
+
+        <div class="profile3">
+            <div class="poto">
+                <img src="{{ asset('ica.jpg') }}" alt="">
+            </div>
+            <div class="nama">
+                <h1>Annisa Haycal</h1>
+            </div>
+            <div class="isi">
+                <h6>Hello, my name is ijah and i'm 22 years old. I am study at bina nusantara.
+                    <br>
+                    My hobby is watching tv and take a shower.
+                    And my favorite course is Web Programming and Chinesse Literature.</h6>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
